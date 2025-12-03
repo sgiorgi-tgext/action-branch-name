49884c41da9ec82c17c62654640b33e1d548a20d
sgiorgi-tgext <sgiorgiwork+tg@gmail.com>
2025-12-03 15:43:56 +0100

    core: Manage "workflow_dispatch" event

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 
-const validEvent = ['push', 'pull_request'];
+const validEvent = ['push', 'pull_request', 'workflow_dispatch'];
 
 function getBranchName(eventName, payload) {
     let branchName;
@@ -11,6 +11,9 @@ function getBranchName(eventName, payload) {
             break;
         case 'pull_request':
             branchName = payload.pull_request.head.ref;
+            break;
+        case 'workflow_dispatch':
+            branchName = payload.ref;
             break;
         default:
             throw new Error(`Invalid event name: ${eventName}`);
